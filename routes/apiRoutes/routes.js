f9f8ffdd31dc62bd42d59458089ead80260a5205
Bader Munir <bader.munir18@googlemail.com>
2022-08-18 23:37:13 +0100

    add get and post routes

--- routes/apiRoutes/routes.js
+++ routes/apiRoutes/routes.js
@@ -11,3 +11,28 @@ if (savedNotes) {
 } else {
   notes = [];
 }
+
+router.get("/notes", (req, res) => {
+    return res.json(notes);
+  });
+
+  router.post("/notes", function (req, res) {
+
+    let noteId = uuidv4();
+  
+    let newNote = {
+      id: noteId,
+      title: req.body.title,
+      text: req.body.text,
+    };
+   
+    console.log(newNote);
+    notes.push(newNote);
+    
+    res.json(newNote);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes, null, 2), function (
+      err
+    ) {
+      if (err) throw err;
+    });
+  });
