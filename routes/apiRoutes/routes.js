f150fca314a0bdf5265999e9b2cb5d4a5e1aaaee
Bader Munir <bader.munir18@googlemail.com>
2022-08-18 23:30:42 +0100

    add function to check saved notes

--- routes/apiRoutes/routes.js
+++ routes/apiRoutes/routes.js
@@ -2,3 +2,12 @@ const { db } = require("../../db/db.json");
 const router = require("express").Router();
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
+
+//check saved notes if exist or not
+const savedNotes = fs.readFileSync("./db/db.json", "UTF-8");
+if (savedNotes) {
+  let oldNotes = JSON.parse(savedNotes);
+  notes = oldNotes;
+} else {
+  notes = [];
+}
